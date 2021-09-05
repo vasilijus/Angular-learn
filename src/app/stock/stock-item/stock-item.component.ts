b11f3401a9b38018e01131839d232e1eadea2261
Sergio <vasiliokas@gmail.com>
2021-09-05 13:29:22 +0100

    stock-item component takes in array of Stock

--- src/app/stock/stock-item/stock-item.component.ts
+++ src/app/stock/stock-item/stock-item.component.ts
@@ -10,24 +10,29 @@ import { Stock } from 'src/app/model/stock';
 
 export class StockItemComponent implements OnInit {
 
-  public stock: Stock;
+  public stocks: Array<Stock>;
   public stockClasses: any;
 
   constructor() {
 
-    this.stock = new Stock('Test Stock Comp', 'TSC', 92, 104);
-    let diff = (this.stock.price / this.stock.previousPrice) - 1;
-    let largeChange = Math.abs(diff) > 0.01;
-    this.stockClasses = {
-      "color": this.stock.isPositiveChange() ? "green" : "red",
-      "font-size": largeChange ? "1.2em" : "0.8em",
-    };
+    this.stocks = [
+      new Stock('Test Stock Comp', 'TSC', 92, 104),
+      new Stock('Test Apple Farm', 'TAF', 12, 24),
+      new Stock('Test Metal Sheets', 'TMS', 52, 74),
+    ];
+
+    // let diff = (this.stock.price / this.stock.previousPrice) - 1;
+    // let largeChange = Math.abs(diff) > 0.01;
+    // this.stockClasses = {
+    //   "color": this.stock.isPositiveChange() ? "green" : "red",
+    //   "font-size": largeChange ? "1.2em" : "0.8em",
+    // };
 
   }
 
-  toggleFavorite(event:any) {
-    console.log('We are togling fav.', event);
-    this.stock.favorite = !this.stock.favorite;
+  toggleFavorite(event:any, index:number) {
+    console.log('We are togling fav.', event, index);
+    this.stocks[index].favorite = !this.stocks[index].favorite;
   }
 
   ngOnInit(): void {
