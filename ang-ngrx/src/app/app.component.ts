e1a410d585adf3a25cf65a369aa42b2f384bf343
Sergio <vasiliokas@gmail.com>
2021-09-06 23:45:53 +0100

    simple counter | date format

--- ang-ngrx/src/app/app.component.ts
+++ ang-ngrx/src/app/app.component.ts
@@ -7,4 +7,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ang-ngrx';
+  counter: number = 0;
+  updatedAt?: number;
+
+  increase() {
+    this.updatedAt = Date.now();
+    this.counter += 1;
+  }
+  decrease() {
+    this.updatedAt = Date.now();
+    this.counter -= 1;
+  }
+  reset() {
+    this.updatedAt = Date.now();
+    this.counter = 0;
+  }
 }
